8e91b1468848359fcb1a4bfaa0800dbd65d0a5fc
nguyen171204 <nguyenhuynhtan700@gmail.com>
2025-10-17 11:47:19 +0700

    Add files via upload

--- jokes.html.php
+++ jokes.html.php
@@ -1,75 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>List of Jokes</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 30px;
-            background-color: #f5f5f5;
-        }
-        h1 {
-            text-align: center;
-            margin-bottom: 25px;
-        }
-        table {
-            border-collapse: collapse;
-            width: 80%;
-            margin: 0 auto;
-            background: #fff;
-            box-shadow: 0 0 5px rgba(0,0,0,0.2);
-        }
-        td, th {
-            border: 1px solid #000;
-            padding: 12px;
-            text-align: center;
-            vertical-align: middle;
-        }
-        img {
-            height: 100px;
-            width: auto;
-        }
-        em {
-            color: #777;
-        }
-    </style>
-</head>
-<body>
+<h2>List of Jokes</h2>
 
-<h1>List of Jokes</h1>
+<?php foreach ($jokes as $joke): ?>
+  <div class="joke-item">
+    <div class="joke-image">
+      <?php
+  $img = !empty($joke['imagepath']) ? $joke['imagepath'] : '/COMP1841/Week5/images/JEJEJEJEJJEJ.jpg';
+        $alt = !empty($joke['imagepath']) ? 'Joke image' : 'Default joke placeholder image';
+      ?>
+      <img src="<?= htmlspecialchars($img, ENT_QUOTES, 'UTF-8') ?>" alt="<?= htmlspecialchars($alt, ENT_QUOTES, 'UTF-8') ?>">
+    </div>
 
-<table>
-    <tr>
-        <th>Joke</th>
-        <th>Date</th>
-        <th>Image</th>
-    </tr>
+    <div class="joke-content">
+      <p class="joke-text"><?= htmlspecialchars($joke['joketext'], ENT_QUOTES, 'UTF-8') ?></p>
+      <p class="joke-date">Posted on: <?= htmlspecialchars($joke['jokedate'], ENT_QUOTES, 'UTF-8') ?></p>
+    </div>
 
-    <tr>
-        <td>Doctor’s joke 😂</td>
-        <td>2025-10-01</td>
-        <td><img src="images/picture1.png" alt="Doctor Joke"></td>
-    </tr>
-
-    <tr>
-        <td>Programmer’s joke 💻</td>
-        <td>2025-10-02</td>
-        <td><img src="images/picture 2.jpg" alt="Programmer Joke"></td>
-    </tr>
-
-    <tr>
-        <td>Teacher’s joke 📚</td>
-        <td>2025-10-03</td>
-        <td><img src="images/picture 3.png" alt="Teacher Joke"></td>
-    </tr>
-
-    <tr>
-        <td>Student’s joke 🎓</td>
-        <td>2025-10-04</td>
-        <td><img src="images/picture 4.jpeg" alt="Student Joke"></td>
-    </tr>
-</table>
-
-</body>
-</html>
+    <div class="joke-actions">
+      <form action="deletejoke.php" method="post">
+        <input type="hidden" name="id" value="<?= $joke['id'] ?>">
+        <input type="submit" value="Delete" class="delete-button">
+      </form>
+    </div>
+  </div>
+  <hr>
+<?php endforeach; ?>
---
+++ jokes.php
@@ -0,0 +1,18 @@
+<?php
+try {
+    include 'includes/DatabaseConnection.php';
+
+    $sql = 'SELECT * FROM joke';
+    $jokes = $pdo->query($sql);
+    $title = 'Joke list';
+
+    ob_start();
+    include 'templates/jokes.html.php';
+    $output = ob_get_clean();
+} catch (PDOException $e) {
+    $title = 'An error has occurred';
+    $output = 'Database error: ' . $e->getMessage();
+}
+
+include 'templates/layout.html.php';
+?>
